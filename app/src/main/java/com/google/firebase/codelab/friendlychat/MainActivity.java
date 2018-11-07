7bb9f496aa3674a736527f3156c980bbf3a0b588
jueunyeo <39024787+jueunyeo@users.noreply.github.com>
2018-11-07 16:46:44 +0900

    commit!

--- app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+//commit!
 package com.google.firebase.codelab.friendlychat;
 
 import android.content.Intent;
